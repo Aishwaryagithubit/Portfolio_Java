2f30b560914a312a105e844c81b971ee8dd2b134
“[firstname <saishwarya23@tbc.edu.np>
2025-07-08 12:35:13 +0545

    initial commit in conditional and loop files

---
+++ src/CheckSalary.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class CheckSalary {
+    public static void main(String[]args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter your salary: ");
+        int salary = s.nextInt();
+        if(salary>100000){
+            salary +=2000;
+        }
+        else{
+           salary +=1000;
+        }
+
+        System.out.println("You recieved salary with bonus:" + "Rs"+salary);
+    }
+}
