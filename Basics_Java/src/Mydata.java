0cebb971e3ba35047f31b9942765b9fef688c18c
Aishwarya Sah <146162125+Aishwaryagithubit@users.noreply.github.com>
2025-07-10 13:13:15 +0545

    files added

---
+++ Basics_Java/src/Mydata.java
@@ -0,0 +1,15 @@
+public class Mydata {
+     public static void main(String[]args){
+         int num = 200000;
+         float num1 = 4572.78f;
+         boolean value = true;
+         String name = "Aishwarya";
+         char Section = 'A';
+
+         System.out.println("Number is " + num);
+         System.out.println("Number1 is " + num1);
+         System.out.println("value is " + value);
+         System.out.println("Name is " + name);
+         System.out.println("Section is " + Section);
+     }
+}
