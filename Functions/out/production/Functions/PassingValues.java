56bcc5a49e04a02f49c8dbe9e24682c0ae63d129
Aishwarya Sah <146162125+Aishwaryagithubit@users.noreply.github.com>
2025-07-22 08:49:07 +0545

    file added

---
+++ Functions/out/production/Functions/PassingValues.java
@@ -0,0 +1,11 @@
+
+public class PassingValues {
+    public static void main(String[] args) {
+        String name = "Aish";
+        greet (name);
+    }
+
+    static void greet(String naam){
+        System.out.println(naam);
+    }
+}
