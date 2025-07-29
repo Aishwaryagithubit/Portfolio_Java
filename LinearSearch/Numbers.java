6aaf5bda5e444106c54f51b5b886756e093167c9
Aishwarya Sah <146162125+Aishwaryagithubit@users.noreply.github.com>
2025-07-29 10:28:26 +0545

    files uploaded

---
+++ LinearSearch/Numbers.java
@@ -0,0 +1,25 @@
+
+public class Numbers {
+    public static void main(String[] args) {
+       int [] nums = {1,32,57,44,67,111,-20};
+       int target = 111;
+       int ans = Findnum(nums,target);
+        System.out.println(ans);
+    }
+
+    //return index 1 otherwise -1
+    static int Findnum(int[]arr , int target){
+        if(arr.length==0){
+            return -1;
+        }
+
+        for(int i = 0; i<arr.length ; i++){
+            //check element
+            int element = arr[i];
+            if(element == target){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
---
+++ LinearSearch/SearchString.java
@@ -0,0 +1,22 @@
+public class SearchString {
+    public static void main(String[] args) {
+        String name = "Durga";
+        char target = 'a';
+        System.out.println(Search(name,target));
+
+
+    }
+
+    static boolean Search(String str, char target){
+        //calling length()
+        if(str.length() == 0){
+            return false;
+        }
+        for(int i = 0 ; i<str.length(); i++){
+            if (target == str.charAt(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
