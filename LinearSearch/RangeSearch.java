445b8882723f1ae7ce37262a203cb7e04d0043f7
Aishwarya Sah <146162125+Aishwaryagithubit@users.noreply.github.com>
2025-07-29 11:36:49 +0545

    file added

---
+++ LinearSearch/RangeSearch.java
@@ -0,0 +1,23 @@
+public class RangeSearch {
+    public static void main(String[] args) {
+     int [] nums = {19,11,-7,30,15,27};
+     int target = 3;
+        System.out.println(rangecheck(nums, target, 1,5));
+    }
+
+    // start to end range
+    static int rangecheck(int [] arr, int target, int start , int end){
+        if(arr.length == 0){
+            return -1;
+        }
+
+        for(int i = start; i<= end ; i++){
+            int element = arr[i];
+            if(element == target){
+                return i;
+            }
+
+        }
+        return -1;
+    }
+}
