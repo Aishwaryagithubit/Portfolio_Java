a3c35539152d09fa3b73ec63853e6b3bbdcc38e4
Aishwarya Sah <146162125+Aishwaryagithubit@users.noreply.github.com>
2025-08-07 13:40:55 +0545

    file added

---
+++ Arrays_Java/ArrayListQuestion.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class ArrayListQuestion {
+    public static void main(String[] args) {
+        //syntax
+        ArrayList<Integer> list = new ArrayList<>(10);
+         list.add(45);
+        list.add(65);
+        list.add(65);
+        list.add(55);
+        list.add(75);
+        System.out.println(list);
+    }
+}
