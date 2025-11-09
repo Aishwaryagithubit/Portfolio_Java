01974a8c021f15fea1ddab07d804ecf4b0867233
Aishwarya Sah <146162125+Aishwaryagithubit@users.noreply.github.com>
2025-11-09 22:18:42 +0545

    Change welcome message in First.java

--- First.java
+++ First.java
@@ -1,5 +1,6 @@
 public class First {
     public static void main(String[]args){
-        System.out.print("Welcome to java programming");
+        System.out.print("Welcome to this java programming");
     }
 }
+
