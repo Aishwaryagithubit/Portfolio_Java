e7ecfa40ff9b296f08a8d6fa3fddd95cdf767a9e
Aishwarya Sah <146162125+Aishwaryagithubit@users.noreply.github.com>
2025-12-07 23:02:58 +0545

    Update ColNofixed.java

--- Arrays_Java/ColNofixed.java
+++ Arrays_Java/ColNofixed.java
@@ -3,7 +3,7 @@ public class ColNofixed {
         int [][] arr = {
                 {1,2,3,4},
                 {5,6},
-                {7,8,9}
+                {7,8,10}
         };
 
         for(int row = 0; row<arr.length ; row++){
@@ -14,3 +14,4 @@ public class ColNofixed {
         }
     }
 }
+
