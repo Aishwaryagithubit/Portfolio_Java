e1c39a793776e63b9098be432b4e1228a3f54b78
Aishwarya Sah <146162125+Aishwaryagithubit@users.noreply.github.com>
2025-12-16 23:24:53 +0545

    Update Inputs.java

--- Inputs.java
+++ Inputs.java
@@ -3,15 +3,16 @@ import java.util.Scanner;
 public class Inputs {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter your name: ");
+        System.out.print("Enter your name : ");
         String name = s.nextLine();
-        System.out.print("Enter your ID: ");
+        System.out.print("Enter your ID : ");
         int id = s.nextInt();
         System.out.println("My name is " + name);
         System.out.println("ID: " + id);
         s.close(); // It's good practice to close the scanner
     }
 }
+
 
 
 
