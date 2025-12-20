e1f32ab8002e15e2d4123ae0ff283ccbad1b2406
Aishwarya Sah <146162125+Aishwaryagithubit@users.noreply.github.com>
2025-12-20 22:31:49 +0545

    file added

---
+++ Arrays_Java/Tax.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Tax {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter your income in lakh");
+        float income = s.nextFloat();
+        if(income>=2.5 && income<5.0){
+            System.out.println("pay tax 5%"+ "for" + income+"L");
+        }
+        else if(income>=5.0 && income<10.0){
+            System.out.println("pay tax 20%"+ "for" + income+"L");
+        }
+        else if(income>=10.0){
+            System.out.println(" pay tax 30%"+ "for" + income+"L");
+        }
+        else{
+            System.out.println("No tax enjoy!!");
+        }
+
+    }
+}
