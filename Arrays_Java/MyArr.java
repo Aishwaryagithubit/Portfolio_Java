a308ed781409180069f31857b58f70d45b4beacc
Aishwarya Sah <146162125+Aishwaryagithubit@users.noreply.github.com>
2025-12-26 23:16:05 +0545

    files added ArrayList

---
+++ Arrays_Java/MyArr.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyArr {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(9);
+        list.add(17);
+        System.out.println(list.get(2));
+        System.out.println(list.size());
+    }
+}
