15e1471492eb24f5eed22cbd539fa1275e469f9a
Aishwarya Sah <146162125+Aishwaryagithubit@users.noreply.github.com>
2026-01-01 15:01:54 +0545

    Update Temperature.java

--- Temperature.java
+++ Temperature.java
@@ -4,10 +4,10 @@ public class Temperature {
    public static void main(String[]args){
        Scanner s = new Scanner(System.in);
        System.out.println("Enter temperature in celsius: ");
-      
        float tempc = s.nextFloat();
        float tempF = (tempc * 9/5) + 32;
        System.out.println(tempF+"F");
    }
 }
+
 
