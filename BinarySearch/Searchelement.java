57a7f66e916e692d40006a1013e3f6ef49f2e09b
Aishwarya Sah <146162125+Aishwaryagithubit@users.noreply.github.com>
2026-01-11 19:59:14 +0545

    Update Searchelement.java

--- BinarySearch/Searchelement.java
+++ BinarySearch/Searchelement.java
@@ -2,7 +2,7 @@ public class Searchelement {
     public static void main(String[] args) {
         
        int[] arr = {2,4,6,7,9,22,34};
-       int target = 6;
+       int target = 5;
        int ans = BinSearch(arr,target);
         System.out.println(ans);
     }
@@ -31,6 +31,7 @@ public class Searchelement {
         return -1;
     }
 }
+
 
 
 
