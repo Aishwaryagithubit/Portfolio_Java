0da4b26da53769a0e80954d6a8bdab32606bf5c0
Aishwarya Sah <146162125+Aishwaryagithubit@users.noreply.github.com>
2026-01-15 20:04:16 +0545

    Update Str_array.java

--- Arrays_Java/Str_array.java
+++ Arrays_Java/Str_array.java
@@ -5,6 +5,7 @@ public class Str_array {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("enter strings: ");
+        
         //declare array name
         String [] str = new String[4];
         for(int i = 0;i<str.length;i++){
@@ -20,5 +21,6 @@ public class Str_array {
     }
 
 }
+
 
 
